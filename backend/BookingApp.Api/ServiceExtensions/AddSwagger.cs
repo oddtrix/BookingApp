634b6fe8d079cece4001384006195a4dbacb7437
qwe <qwe@MYPC>
2026-08-19 13:45:06 +0300

    Add project files.

---
+++ backend/BookingApp.Api/ServiceExtensions/AddSwagger.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.OpenApi;
+
+namespace BookingApp.Api.ServiceExtensions
+{
+    public static class AddSwagger
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "BookingApp", Version = "v1" });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ backend/BookingApp.Api/Startup.cs
@@ -0,0 +1,35 @@
+﻿using BookingApp.Api.ServiceExtensions;
+
+namespace BookingApp.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerConfiguration();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
