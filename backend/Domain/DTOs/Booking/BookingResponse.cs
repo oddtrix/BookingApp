72cc3cb5f0790a83b941b6803ace9a602f5a85b9
Vladislav <vladyslav.ilnytskyi.dev@gmail.com>
2026-08-19 19:39:32 +0300

    Add Booking dtos

---
+++ backend/Domain/DTOs/Booking/BookingResponse.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Enums;
+
+namespace Domain.DTOs.Booking
+{
+    public class BookingResponse
+    {
+        public Guid Id { get; set; }
+
+        public Guid ResourceId { get; set; }
+
+        public string ResourceName { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public string Error { get; set; } = string.Empty;
+    }
+}
---
+++ backend/Domain/DTOs/Booking/CreateBookingRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.DTOs.Booking
+{
+    public class CreateBookingRequest
+    {
+        public Guid ResourceId { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+    }
+}
