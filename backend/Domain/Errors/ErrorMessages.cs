5a3e56241428bbf9a6077e4c7d4ac14eba92fde4
Vladislav <vladyslav.ilnytskyi.dev@gmail.com>
2026-08-19 19:39:10 +0300

    Move error messages to the separate file

---
+++ backend/Domain/Errors/ErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Errors
+{
+    public static class ErrorMessages
+    {
+        public const string EndTimeMustBeGreaterThanStartTime = "EndTime must be greater than StartTime.";
+        public const string ResourceNotFound = "Resource not found.";
+        public const string ResourceIsInactive = "Resource is inactive.";
+        public const string SelectedSlotIsBooked = "The selected time slot is already booked.";
+        public const string UsernameIsRequired = "Username is required.";
+
+        public const string InvalidResource = "Invalid resource. Please check your data again.";
+    }
+}
